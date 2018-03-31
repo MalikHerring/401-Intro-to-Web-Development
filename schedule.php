328c2296855d8390a4c15fa333f5a59ab514e172
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-03-31 11:28:54 -0700

    Commit from Makefile

--- schedule.php
+++ schedule.php
@@ -14,14 +14,23 @@
     <div id = content>
 	<?php echo $heading; ?>
         <div id="mainBox">
-            <p> This will be where we find appointments </p>
+            <?php
+                echo "<tr><th>Username</th><th>Email</th><th>Password</th><th>Access</th><th>Account ID</th><tr>";
+                foreach($users as $user) {
+                    print   "<tr><td>" . htmlspecialchars($user['username']) . "</td>" .
+                            "<td>" . htmlspecialchars($user['email']) . "</td>" .
+                            "<td>" . htmlspecialchars($user['password']) . "</td>" .
+                            "<td>" . $user['access'] . "</td>" .
+                            "<td>" . $user['accountID'] . "</td>";
+                }
+            ?>
         </div>
         <div id="sidebar">
             <form action="handler.php" method="POST" enctype="multipart/form-data">
-                <div> Username: <input type="text" id="username" name="username"></div>
-                <div> Email: <input type="text" id="email" name="email"></div>
-                <div> Password: <input type="password" id="password" name="password"></div>
-                <div> Confirm Password: <input type="password" id="confirmPass" name="confirmPassword"></div>
+                <div> Username: <br/><input type="text" id="username" name="username"></div>
+                <div> Email: <br/><input type="text" id="email" name="email"></div>
+                <div> Password: <br/><input type="password" id="password" name="password"></div>
+                <div> Confirm Password:<br/><input type="password" id="confirmPass" name="confirmPassword"></div>
                 <div><input type="submit" value="Create User"></div>
             </form>
         </div>
