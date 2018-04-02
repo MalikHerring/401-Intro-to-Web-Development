ee80d0eef86f42cb653531464a735915b62b9f03
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-02 13:44:57 -0700

    Commit from Makefile

--- schedule.php
+++ schedule.php
@@ -19,18 +19,20 @@
             <p> Will display Day, Week, Month, Year schedule </p>
         </div>
         <div id="sidebar">
-            <?php if(isset($username){ ?>
-            <div>
-                <h1>Welcome back, <?php $username ?> </h1>
-            <?php } else { ?>
-            <form action="createHandler.php" method="POST" enctype="multipart/form-data">
-                <div> Username: <br/><input type="text" id="username" name="username"></div>
-                <div> Email: <br/><input type="text" id="email" name="email"></div>
-                <div> Password: <br/><input type="password" id="password" name="password"></div>
-                <div> Confirm Password:<br/><input type="password" id="confirmPass" name="confirmPassword"></div>
-                <div><input type="submit" value="Create User"></div>
-            </form>"
-            <?php } ?>
+            <?php if(isset($username){ 
+            echo 
+                "<div>" .
+                    "<h1>Welcome back, " . $username . "</h1>" .
+                "</div>"
+            } else { 
+                "<form action=\"createHandler.php\" method=\"POST\" enctype=\"multipart/form-data\">
+                    <div> Username: <br/><input type=\"text\" id=\"username\" name=\"username\"></div>
+                    <div> Email: <br/><input type=\"text\" id=\"email\" name=\"email\"></div>
+                    <div> Password: <br/><input type=\"password\" id=\"password\" name=\"password\"></div>
+                    <div> Confirm Password:<br/><input type=\"password\" id=\"confirmPass\" name=\"confirmPassword\"></div>
+                    <div><input type=\"submit\" value=\"Create User\"></div>
+                </form>"
+            ?>
         </div>
     </div>
 	<div id="footer">
