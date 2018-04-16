ef9eadf096a9a448d526c99d03cec2015f1a0acd
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-16 13:16:33 -0700

    Commit from Makefile

--- createHandler.php
+++ createHandler.php
@@ -1,5 +1,4 @@
 <?php
-    try{
     session_start();
     require_once 'Dao.php';
     $dao = new Dao();
@@ -14,14 +13,34 @@
     $valid = true;
     $messages = array();
     
-    if (empty($username)){
+    if (empty(username)){
         $messages[] = "Please provide a username.";
         $valid = false;
     }
-    if($dao->doesUserExist($email, $username)){
-        $messages[] = "User Already Exists with that Username or Email.";
-        $valid = false;
+        $users = $dao->getUsers();
+    foreach($users as $user){
+        if(strcmp($user['username'], $username) == 0){
+            $messages[] = "Username already taken, Please provide a username";
+            $valid = false;
+            break;
+        } 
+        if (strcmp($user['email'], $email) == 0) {
+            $messages[] = "Email is already in use, please provide a different email";
+            $valid = false;
+            break;
+        }
     }
+ #   if ($username == $user['username']){
+ #       $messages[] = "Username already taken, Please provide a username";
+ #       $valid = false;
+ #   } else {
+ #       $messages[] = "it made it here";
+ #       $messages[] = $username . "<- username";
+ #       $messages[] = $user['username'] . "<- user username";
+ #       $messages[] = $user;
+ #       $valid = false;
+ #   }
+        
     if (strlen($username) > 16){
         $messages[] = "Username cannot be more than 16 characters long";
         $valid = false;
@@ -31,6 +50,7 @@
         $messages[] = "Please provide an email";
         $valid = false;
     }
+    
     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $messages[] = "Invalid Email Format";
         $valid = false;
@@ -52,15 +72,10 @@
         $messages[] = "Passwords do not match";
         $valid = false;
     }
-
     
     if(!$valid){
-        try {
         $_SESSION['validity'] = "invalid";
         $_SESSION['messages'] = $messages;
-        } catch (Exception $e) {
-            echo "Something went wrong: " . e->getMessage();
-        }
         header("Location: index.php");
         exit;
     }
@@ -70,14 +85,7 @@
     $_SESSION['username'] = $username;
     
     $dao->saveUser($username, $email, $password);
-    } catch (Exception $e) {
-        echo "Something went wrong: " . e->getMessage();
-    }
     
     header("Location: index.php");
     exit;
-
 ?>
-    
-
-    
