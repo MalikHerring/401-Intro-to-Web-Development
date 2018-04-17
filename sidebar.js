38bfcc0e1199fc28cc816b4ee6c4ec80c10e2a40
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-17 07:25:51 -0700

    Commit from Makefile

--- sidebar.js
+++ sidebar.js
@@ -1,17 +1,22 @@
-//$(document).ready(function() {
-//    $("#Login").hide();
-//    $("#CreateAccount").hide();
-//    $("#Cancel").hide();
-//});
+$(document).ready(function() {
+    $("#Login").hide();
+    $("#CreateAccount").hide();
+    $("#Cancel").hide();
 
-$("div#loginButton.button").click(function(){
-   $("#Login").show();
-   $(".button").hide();
-   $("#Cancel").show();
+    $("div#loginButton.button").click(function(){
+        $("#Login").show();
+        $(".button").hide();
+        $("#Cancel").show();
+    });
+
+    $("div#createButton.button").click(function(){
+        $("#CreateAccount").show();
+        $(".button").hide();
+        $("#Cancel").show();
+    });
+
 });
 
-$("div#createButton.button").click(function(){
-   $("#CreateAccount").show();
-   $(".button").hide();
-   $("#Cancel").show();
-});
+
+
+
