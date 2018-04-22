8198e887df097219c3fd7f30c038cb11613a3095
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-22 13:04:31 -0700

    Quick Makefile push for testing or aesthetic reasons

--- interaction.js
+++ interaction.js
@@ -9,8 +9,22 @@ $(document).ready(function() {
         $(this).css("background-color", "#50A2A7");
         $("div#createButton.button").css("background-color", "#004BA8");
     });
+    
+    $("ul#horizontal-menu li.login").click(function(){
+        $("#CreateAccount").hide();    
+        $("#Login").show();
+        $(this).css("background-color", "#50A2A7");
+        $("div#createButton.button").css("background-color", "#004BA8");
+    });
 
     $("div#createButton.button").click(function(){
+        $("#Login").hide();
+        $("#CreateAccount").show();
+        $(this).css("background-color", "#50A2A7");
+        $("div#loginButton.button").css("background-color", "#004BA8");
+    });
+    
+    $("ul#horizontal-menu li.signup").click(function(){
         $("#Login").hide();
         $("#CreateAccount").show();
         $(this).css("background-color", "#50A2A7");
