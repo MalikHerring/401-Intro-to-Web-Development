903342e15c7c7d9e450c0c1c573d73d322c3e7cb
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-22 13:25:02 -0700

    Quick Makefile push for testing or aesthetic reason

--- phpInfo.php
+++ phpInfo.php
@@ -6,8 +6,8 @@
     if ($dao->checkAccess($_SESSION['username')) {    
         phpinfo();
     } else {
-        echo "<script type='text/javascript'>alert(\"You aren't allowed to be here\");</script>";
         header("Location: index.php");
         exit;
     }
+    include("footer.php");
 ?>
