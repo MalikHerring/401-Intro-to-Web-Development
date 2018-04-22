16315ae51ed2c78a2d13720ca023b8715e1370c9
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-22 09:49:27 -0700

    Commit from Makefile

--- header.php
+++ header.php
@@ -1,27 +1,24 @@
 <html>
     <head>
-        <title>Track - Life Assistant</title>
+        <title>Life Assistant</title>
         <link rel="stylesheet" href="style.css">
         <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Gugi" rel="stylesheet">
         <script src="jquery-3.3.1.slim.js"></script>
         <script src="sidebar.js"></script>
     </head>
     <body>
     <div id = content>
+        <div id="header">
+            <img alt= "Running Header" src= "/images/header.jpeg"> 
+            <img alt= "logo" src= "/images/logo.jpeg" id="logo">
+        </div>
         <div id="menubar">
             <ul id="horizontal-menu" id="menuText">
-                <li><a id="logo" href="index.php" alt="Logo"> TRACK </a> </li>
                 <li<?php if ($thisPage == "index") { echo " id =\"currentPage\"";}?>><a href="index.php" alt="Home">Home</a></li>
                 <li<?php if ($thisPage == "schedule") { echo " id =\"currentPage\"";}?>><a href="schedule.php" alt="Schedule">Schedule</a></li>
                 <li<?php if ($thisPage == "account") { echo " id =\"currentPage\"";}?>><a href="account.php" alt="Account">Account</a></li>
-                <?php if(isset($_SESSION['username')){ $username=$_SESSION['username'];?>
-                <li class="signup" id="logout"><a href="logoutHandler.php" alt="logout">Logout</a></li>
-                <li class="login" id="username"><a href="account.php" alt="<?php echo $username;?>"><?php echo $username;?></a></li>
-                <?php } else { ?>
-                <li class="signup" id="signup"><a href="login.php" alt="Sign-up">Sign-up</a></li>
-                <li class="login" id="login"><a href="login.php" alt="Login">Login</a></li>
-                <?php } ?>
+                <li id="signup"><a href="login.php" alt="Sign-up">Sign-up</a></li>
+                <li id="login"><a href="login.php" alt="Login">Login</a></li>
             </ul>
         </div>
