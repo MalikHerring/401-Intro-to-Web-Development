1ba13947ee2d9b4464491778c4b7afc5bc7153c2
MALIKHERRING <MalikHerring@u.boisestate.edu>
2018-04-22 09:56:30 -0700

    Commit from Makefile

--- phpInfo.php
+++ phpInfo.php
@@ -1,4 +1,13 @@
-<?php 
-    include_once( index.html ); 
-    phpinfo();
+<?php
+    session_start();
+    require_once 'Dao.php';
+    $dao=new Dao();
+    include("header.php");
+    if ($dao->checkAccess($_SESSION['username')) {    
+        phpinfo();
+    } else {
+        echo "<script type='text/javascript'>alert(\"You aren't allowed to be here\");</script>";
+        header("Location: index.php");
+        exit;
+    }
 ?>
